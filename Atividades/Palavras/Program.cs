458e7c0971d806aaf525d573e81e8e347eaec5b2
Felipe Coldebella de Assis <466844@unoesc.lan>
2026-08-05 22:14:40 -0300

    05/08/2026 - pag 14 a 17

---
+++ Atividades/Palavras/Program.cs
@@ -0,0 +1,19 @@
+﻿string firstName = "Bilbo";
+string lastName = "Bolseiro";
+
+string note = $"{lastName.ToUpper()} {firstName}";
+
+string initials = $"{lastName[0]} {firstName[0]}";
+
+Console.WriteLine(initials);
+
+//Formtação de strings
+
+string texto = string.Format(
+    "{0} {1} nascido em {2}",
+    firstName,
+    lastName,
+    "1984"
+);
+
+System.Console.WriteLine(texto);
