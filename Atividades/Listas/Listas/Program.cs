c53351e925fed09d54bf266368dbb380e7ef538a
Felipe Coldebella de Assis <466844@unoesc.lan>
2026-08-19 22:07:21 -0300

    Listas

---
+++ Atividades/Listas/Listas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+// ArrayLists
+
+ArrayList arrayList = new ArrayList();
+
+// Adicionando itens à Lista
+arrayList.Add(5);
+
+// Adicionando quantias de itens à Lista
+arrayList.AddRange(new int[] { 1, 2, 3 });
+
+// Insere o valor 7.8 na posição 3 da lista
+arrayList.Insert(3, 7.8);
+
+//Percorrendo itens da lista de forma genérica
+foreach (object obj in arrayList)
+{
+    System.Console.WriteLine(obj);
+}
