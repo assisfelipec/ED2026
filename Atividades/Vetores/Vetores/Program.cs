9f60d308cb3c341571ed6df51764427d51bd18e3
Felipe Coldebella de Assis <466844@unoesc.lan>
2026-08-19 21:38:04 -0300

    Matrizes

--- Atividades/Vetores/Vetores/Program.cs
+++ Atividades/Vetores/Vetores/Program.cs
@@ -6,8 +6,9 @@ Sintaxe: Type[] name;
 // Declaração sem inicialização
 // Obs.. variavél alocada mas nula
 using System.Runtime.ConstrainedExecution;
+using System.Globalization;
 
-int[] numbers;
+/* int[] numbers;
 
 // Inicializando o array
 numbers = new int[5];
@@ -58,3 +59,29 @@ for(int i = 0; i < array.Length; i++)
 
 System.Console.WriteLine($"Valor acumulado do array: {Total}");
 
+*/
+
+// Exemplo dos nomes dos meses do ano
+string[] months = new string[12];
+
+for(int i = 1; i <= 12; i++)
+{
+    DateTime firstDay =
+     new DateTime(DateTime.Now.Year, i, 1);
+
+    DateTime LastDayMonthBefore = 
+     firstDay.AddDays(-1);
+
+    string monthName = 
+     firstDay.ToString(
+            "MMMM",
+            CultureInfo.CreateSpecificCulture("pt-BR")
+        );
+    
+    months[i - 1] = monthName;
+}
+
+foreach(string monthName in months)
+{
+    System.Console.WriteLine($"---> {monthName}");
+}
--- Atividades/Vetores/Vetores/obj/Debug/net10.0/Vetores.AssemblyInfo.cs
+++ Atividades/Vetores/Vetores/obj/Debug/net10.0/Vetores.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vetores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+458e7c0971d806aaf525d573e81e8e347eaec5b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+638e2f512cf5887090feb63a7b5fa230b87d40fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vetores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vetores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
