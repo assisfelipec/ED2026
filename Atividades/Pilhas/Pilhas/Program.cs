436e35126a87bd92f5680199cb98be1a0baa2aa8
Felipe Coldebella de Assis <466844@unoesc.lan>
2026-09-23 22:00:49 -0300

    Hanoi Tower

---
+++ Atividades/Pilhas/Pilhas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+Stack<char> stack = new Stack<char>();
+
+string text = Console.ReadLine()!;
+
+string reversedText = string.Empty;
+
+foreach(char c in text)
+{
+    stack.Push(c);
+}
+
+while(stack.Count > 0)
+{
+    reversedText += stack.Pop();
+}
+
+string isPalindromo =
+    text == reversedText ?
+        "é palindromo" :
+        "não é palindromo";
+
+System.Console.WriteLine(isPalindromo);
